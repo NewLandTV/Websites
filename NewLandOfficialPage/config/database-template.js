4f58dfc6afbbde355230802a3f52db3ba252ed39
JkhTV <candzzang5283@gmail.com>
2026-02-24 11:27:24 +0900

    Upload database.js template

---
+++ NewLandOfficialPage/config/database-template.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+let db_info = {
+    host : '',
+    port : '',
+    user : '',
+    password : '',
+    database : ''
+}
+
+module.exports = {
+    init: function() {
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn) {
+        conn.connect(function(err) {
+            if (err) console.error('mysql connection error : ' + err);
+            else console.log('mysql is connected successfully!');
+        });
+    }
+}
